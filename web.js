dd48d799523e27544297f803b72f398e733703cd
Victor Luiz de Freitas <victor.freitas@entelgy.com>
2019-12-12 18:27:09 -0300

    deploy app to heroku

--- web.js
+++ web.js
@@ -1,4 +1,27 @@
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/src/')); //aqui você define onde está o index.html da sua aplicação.
-app.listen(process.env.PORT || 3000);
+// var express = require('express');
+// var app = express();
+// app.use(express.static(__dirname + '/src/')); //aqui você define onde está o index.html da sua aplicação.
+// app.listen(process.env.PORT || 3000);
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 8000;
+const server = require('http').Server(app);
+
+app.use(express.static(__dirname, 'dist', { index: false }));
+
+
+server.listen(port, function () {
+    console.log("App running on port " + port);
+})
+
+// PathLocationStrategy
+
+app.get('', function (req, res) {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
