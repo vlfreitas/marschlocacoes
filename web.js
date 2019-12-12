2188270b648350818544149e5c3426cecfd1c6f6
Victor Luiz de Freitas <victor.freitas@entelgy.com>
2019-12-12 18:58:06 -0300

    deploy app to heroku

--- web.js
+++ web.js
@@ -1,4 +1,14 @@
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/src/')); //aqui você define onde está o index.html da sua aplicação.
-app.listen(process.env.PORT || 8000);
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files....
+app.use(express.static(__dirname + '/dist/marchlocacoes'));
+
+// Send all requests to index.html
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/marchlocacoes/index.html'));
+});
+
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
