4dc932bd054bdb8e24e83e9ce98ffee84d43a806
Victor Luiz de Freitas <victor.freitas@entelgy.com>
2020-06-25 14:05:34 -0300

    Add server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Install express server
 const express = require('express');
 const path = require('path');
+const http = require('http');
 
 const app = express();
 
@@ -12,5 +13,12 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/marschlocacoes/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
+// var server = http.createServer(app);
+
+// server.listen(app.get('port'), function() {
+//     console.log('Express HTTP server listening on port ' .
+//         red + app.get('port'));
+// });
+
+
 app.listen(process.env.PORT || 8080);
