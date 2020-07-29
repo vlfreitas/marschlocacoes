d2532816107e5ff768f345768569f71c4b9206fb
Victor Luiz de Freitas <victor.freitas@entelgy.com>
2020-07-29 14:13:23 -0300

    Voltando antigas configs

--- web.js
+++ web.js
@@ -1,7 +1,6 @@
 // Install express server
 const express = require('express');
 const path = require('path');
-const http = require('http');
 
 const app = express();
 
@@ -13,12 +12,5 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/marschlocacoes/index.html'));
 });
 
-// var server = http.createServer(app);
-
-// server.listen(app.get('port'), function() {
-//     console.log('Express HTTP server listening on port ' .
-//         red + app.get('port'));
-// });
-
-
-app.listen(process.env.PORT || 8080);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8081);
